d6d236b6d9afb526fd51afa425968ef11cd6c7f4
oksanalutsyk <oksanalutsyk95@gmail.com>
2020-07-15 17:00:28 +0300

    smal changes

--- src/app/shared/guards/product-edit.guard.ts
+++ src/app/shared/guards/product-edit.guard.ts
@@ -7,7 +7,7 @@ import { ProductEditComponent } from '../../components/products/product-edit/pro
 @Injectable()
 export class ProductEditGuard implements CanDeactivate<ProductEditComponent> {
   constructor() {}
-  canDeactivate(component: ProductEditComponent,): boolean {
+  canDeactivate(component: ProductEditComponent): boolean {
     if(component.editStatus ){
       return !component.showMessage() || confirm('Navigate away and SAVE all changes?')
     }
